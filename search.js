c18b44e7aedd4276c7892351e491ae8312187ede
Martin Kadlec <harou2@gmail.com>
2013-02-03 22:10:47 +0100

    universal adding icons

--- search.js
+++ search.js
@@ -1,26 +1,44 @@
 (function() {
 
-	var wnd = document.createElement('div');
-	wnd.style.cssText = 'position: absolute; background: white; border: 3px solid #ccc; min-width: 80px; padding: 10px; height: 15px; border-radius: 4px; box-shadow: 0 0 5px black;';
+	var wnd = document.createElement('unknown');
+	wnd.style.cssText = 'display: block; z-index: 99999999; position: absolute; word-spacing: 5px; background: white; border: 3px solid #ccc; min-width: 100px; text-align: center; padding: 10px; height: 15px; border-radius: 4px; box-shadow: 0 0 5px black;';
 
-	var lnk = document.createElement('a');
-	lnk.innerHTML = '<img src="https://www.google.com/favicon.ico">';
-	wnd.appendChild(lnk);
+	var icons = [
+		{
+			icon: 'https://www.google.com/favicon.ico',
+			url: 'http://www.google.com/search?q=%s&sourceid=opera&num=%i&ie=utf-8&oe=utf-8'
+		},
+		{
+			icon: 'http://en.wikipedia.org/favicon.ico',
+			url: 'http://en.wikipedia.org/wiki/Special:Search?search=%s'
+		},
+		{
+			icon: 'http://s1.ikiwq.com/shared/img/favicon.png',
+			url: 'http://www.qwiki.com/search?query=%s&watch=watch'
+		},
+		{
+			icon: 'http://translate.google.com/favicon.ico',
+			url: 'http://translate.google.com/#en/cs/%s'
+		}
+	];
 
-	wnd.appendChild(document.createTextNode(' '));
-
-	var lnkWiki = document.createElement('a');
-	lnkWiki.innerHTML = '<img src="http://en.wikipedia.org/favicon.ico">';
-	wnd.appendChild(lnkWiki);
-
+	var i;
+	for (i=0; i<icons.length; i++) {
+		icons[i].lnk = document.createElement('a');
+		icons[i].lnk.innerHTML = '<img src="' + icons[i].icon + '">';
+		icons[i].lnk.target = '_blank';
+		wnd.appendChild(icons[i].lnk);	
+		if (i + 1 < icons.length) {
+			wnd.appendChild(document.createTextNode(' '));
+		}
+	}
 
 	document.addEventListener('mouseup', function(e) {
 		var txt = window.getSelection().toString().trim();
 		if (txt && !wnd.parentNode) {
-			lnk.href = 'http://www.google.com/search?q=%s&sourceid=opera&num=%i&ie=utf-8&oe=utf-8'.replace('%s', txt);
-			lnkWiki.href = 'http://en.wikipedia.org/wiki/Special:Search?search=%s'.replace('%s', txt);
-			wnd.style.left = e.pageX - 40 + 'px';
-			wnd.style.top = e.pageY - 55 + 'px';
+			setLinks(txt);
+			wnd.style.left = (e.pageX - 50 < 0 ? 0 : e.pageX - 50) + 'px';
+			wnd.style.top = (e.pageY - 55 < 0 ? 0 : e.pageY - 55) + 'px';
 			document.body.appendChild(wnd);
 		} 
 	});
@@ -30,6 +48,12 @@
 			document.body.removeChild(wnd);
 		}
 	});
+
+	function setLinks(txt) {
+		for (var i=0; i<icons.length; i++) {
+			icons[i].lnk.href = icons[i].url.replace('%s', txt);
+		}
+	}
 
 	function isAncestor(what, test) {
 		if (what.isSameNode(test)) {
